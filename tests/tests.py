2cd5951c7d609f95b5d9b21aabd7cfdc1f43114f
Hugo <hugovk@users.noreply.github.com>
2020-02-14 11:36:35 +0200

    Fix DeprecationWarning: Please use assertEqual instead

--- tests/tests.py
+++ tests/tests.py
@@ -696,7 +696,7 @@ class UltraJSONTests(unittest.TestCase):
 
         output = ujson.encode(ObjectTest())
         dec = ujson.decode(output)
-        self.assertEquals(dec, {})
+        self.assertEqual(dec, {})
 
     def test_toDict(self):
         d = {"key": 31337}
